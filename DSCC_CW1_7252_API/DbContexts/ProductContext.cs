982a1cad0adf3f6dcd2841e41677cc7eb5620833
00007252 <bsalimov2@students.wiut.uz>
2020-10-29 16:43:04 +0500

    My First Commit

---
+++ DSCC_CW1_7252_API/DbContexts/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using DSCC_CW1_7252_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSCC_CW1_7252_API.DbContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ DSCC_CW1_7252_API/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DSCC_CW1_7252_API.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
